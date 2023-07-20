0a855fba046151aa8bde389b2b4d1dee2501f897
Lê Quốc Huy <97956803+QuocHuy03@users.noreply.github.com>
2023-07-20 12:32:31 +0700

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,46 +1,3 @@
-const User = require("../models/user");
-const random = require("random-token");
-
-// register
-exports.createUser = (req, res, next) => {
-  const { fullname, password, repassword, email } = req.body;
-  if (fullname == "" || password == "" || repassword == "" || email == "") {
-    res.status(200).json({ status: false, message: "Không Được Để Trống" });
-  } else {
-    if (password !== repassword) {
-      return res
-        .status(200)
-        .json({ status: false, message: "Mật khẩu nhập lại không khớp!" });
-    } else {
-        var token = random(24);
-        const users = new User({
-          fullname,
-          password,
-          email,
-          token: token,
-        });
-        console.log("Register : ", users);
-        users
-          .save()
-          .then((result) => {
-            res.status(201).json({
-              status: true,
-              message: "Đăng Ký Thành Công",
-              info: result,
-            });
-          })
-          .catch((err) => {
-            if (!err.statusCode) {
-              err.statusCode = 500;
-            }
-            next(err);
-          });
-        }
-        
-  }
-};
-// login
-
 exports.loginUser = (req, res, next) => {
   const { email, password } = req.body;
   if (email == "" || password == "") {
@@ -73,14 +30,13 @@ exports.loginUser = (req, res, next) => {
               .status(200)
               .json({ status: false, message: "Đăng nhập thất bại" });
           }
-        }
-      })
+        })
+        .catch((err) => {
+          if (!err.statusCode) {
+            err.statusCode = 500;
+          }
+          next(err);
+        });
+      }
 
-      .catch((err) => {
-        if (!err.statusCode) {
-          err.statusCode = 500;
-        }
-        next(err);
-      });
   }
-};
