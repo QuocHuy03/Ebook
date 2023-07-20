f6945dd9859d2852e240e564fac578c3274d8bfc
Lê Quốc Huy <97956803+QuocHuy03@users.noreply.github.com>
2023-07-20 12:48:05 +0700

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,49 @@
+exports.createUser = (req, res, next) => {
+  const { fullname, password, repassword, email } = req.body;
+  if (fullname == "" || password == "" || repassword == "" || email == "") {
+    res.status(200).json({ status: false, message: "Không Được Để Trống" });
+  } else {
+    if (password !== repassword) {
+      return res
+        .status(200)
+        .json({ status: false, message: "Mật khẩu nhập lại không khớp!" });
+    } else {
+      // Mã hóa mật khẩu
+      bcrypt.hash(password, 10, (err, hash) => {
+        if (err) {
+          return res.status(500).json({
+            status: false,
+            message: "Có lỗi xảy ra trong quá trình mã hóa mật khẩu",
+          });
+        }
+        var token = random(24);
+        const users = new User({
+          fullname,
+          password: hash,
+          email,
+          token: token,
+        });
+        console.log("Register : ", users);
+        users
+          .save()
+          .then((result) => {
+            res.status(201).json({
+              status: true,
+              message: "Đăng Ký Thành Công",
+              info: result,
+            });
+          })
+          .catch((err) => {
+            if (!err.statusCode) {
+              err.statusCode = 500;
+            }
+            next(err);
+          });
+      });
+    }
+  }
+};
+
 exports.loginUser = (req, res, next) => {
   const { email, password } = req.body;
   if (email == "" || password == "") {
